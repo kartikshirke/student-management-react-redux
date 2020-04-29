f8995f5148fd011e8e41eef3f2aef3825ae0e0d6
Kartik_shirke <Kartikdeepakshirke@gmail.com>
2020-04-29 18:00:12 -0400

    Add files via upload

---
+++ build/precache-manifest.438ffcff4d20e2572c758f94bff2bae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2f1dd13afe01f4337b486cdf1dac96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b449bd30179c75e67452",
+    "url": "/static/css/main.fb68b768.chunk.css"
+  },
+  {
+    "revision": "4e05b5272eb1a7a12c48",
+    "url": "/static/js/2.1c753867.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c753867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b449bd30179c75e67452",
+    "url": "/static/js/main.b2fba7ad.chunk.js"
+  },
+  {
+    "revision": "ab4a53677318f2927241",
+    "url": "/static/js/runtime-main.829bc384.js"
+  }
+]);

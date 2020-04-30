467b12a91f7931f0a5fadc583aab734fe7d81389
Kartik_shirke <Kartikdeepakshirke@gmail.com>
2020-04-30 09:10:30 -0400

    Add files via upload

--- src/AllPost.js
+++ src/AllPost.js
@@ -10,51 +10,56 @@ class AllPost extends Component {
 
     render() {
         return (
-            <div className="table">
+            <div className="post-container">
+                 <h1 className="post_heading">All Students Record</h1>
+                <div className="table">
 
-                <table style={{ width: '50%' }}>
-                    <tr>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Phone</th>
-                        <th>GPA</th>
-                        <th>Delete</th>
-                    </tr>
+                    <table style={{ width: '50%' }}>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Phone</th>
+                            <th>GPA</th>
+                            <th>Delete</th>
+                        </tr>
 
-                    {
-                        this.props.posts.map(el => (
+                        {
+                            this.props.posts.map(el => (
 
-                            <tr>
-                                <td style={{ cursor: "pointer" }} onClick={(e) => {
-                                    e.preventDefault();
-                                    this.props.dispatch({ type: 'REVIEW_POST', id: el.id })
-                                    this.props.history.push('/show');
-                                }}>{el.firstName}</td>
-                                <td style={{ cursor: "pointer" }} onClick={(e) => {
-                                    e.preventDefault();
-                                    this.props.dispatch({ type: 'REVIEW_POST', id: el.id })
-                                    this.props.history.push('/show');
-                                }}>{el.lastName}</td>
-                                <td style={{ cursor: "pointer" }} onClick={(e) => {
-                                    e.preventDefault();
-                                    this.props.dispatch({ type: 'REVIEW_POST', id: el.id })
-                                    this.props.history.push('/show');
-                                }}>{el.phone}</td>
-                                <td style={{ cursor: "pointer" }} onClick={(e) => {
-                                    e.preventDefault();
-                                    this.props.dispatch({ type: 'REVIEW_POST', id: el.id })
-                                    this.props.history.push('/show');
-                                }}>{el.gpa}</td>
-                                <td>
-                                    <span className="control-buttons">
-                                        <button className="delete"
-                                            onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: el.id })}>Delete</button>
-                                    </span>
-                                </td>
-                            </tr>
-                        ))}
-                </table>
-
+                                <tr>
+                                    <td style={{ cursor: "pointer" }} onClick={(e) => {
+                                        e.preventDefault();
+                                        this.props.dispatch({ type: 'REVIEW_POST', id: el.id })
+                                        this.props.history.push('/show');
+                                    }}>{el.firstName}</td>
+                                    <td style={{ cursor: "pointer" }} onClick={(e) => {
+                                        e.preventDefault();
+                                        this.props.dispatch({ type: 'REVIEW_POST', id: el.id })
+                                        this.props.history.push('/show');
+                                    }}>{el.lastName}</td>
+                                    <td style={{ cursor: "pointer" }} onClick={(e) => {
+                                        e.preventDefault();
+                                        this.props.dispatch({ type: 'REVIEW_POST', id: el.id })
+                                        this.props.history.push('/show');
+                                    }}>{el.phone}</td>
+                                    <td style={{ cursor: "pointer" }} onClick={(e) => {
+                                        e.preventDefault();
+                                        this.props.dispatch({ type: 'REVIEW_POST', id: el.id })
+                                        this.props.history.push('/show');
+                                    }}>{el.gpa}</td>
+                                    <td>
+                                        <span className="control-buttons">
+                                            <button className="delete"
+                                                onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: el.id })}>Delete</button>
+                                        </span>
+                                    </td>
+                                </tr>
+                            ))}
+                            <br/><br/>
+                            <button className="button-class" onClick={()=> this.props.history.push('/')}>Home</button>
+                    </table>
+                    
+                </div>
             </div>
         );
     }
